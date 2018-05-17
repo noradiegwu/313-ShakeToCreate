cf537d178b5621cda8d3c05eb2c55652b198f446
noradiegwu <nneomaoradiegwu2019@u.northwestern.edu>
2018-05-17 13:13:20 -0500

    measurement descriptions

---
+++ data/measurements.py
@@ -0,0 +1,21 @@
+measurements = dict()
+
+measurements['bunch'] = "a collection of the ingredient, growing, fastened, or grouped together";
+measurements['can'] = "a can is the entirety of the can of your ingredient. Most often packaged in 8 or 16 ounces";
+measurements['clove'] = "a clove is 1 teaspoon of your chopped ingredient or half a teaspoon of your minced ingredient. This measurement is most often associated with garlic.";
+measurements['container'] = "a container is whatever holds the entirety of the ingredient";
+measurements['envelope'] = "the amount of material in an envelope, or packet, varies. Refer to your list of ingredients for the exact measurement";
+measurements['head'] = "a head usually refers to a single, intact, dense rosette of leaves which ultimately develops into a compact head. The weight of which is, on average, about one pound.";
+measurements['teaspoon'] = "a teaspoon is one third of a tablespoon. You can most often use one half of a small spoon to replicate this measurement.";
+measurements['tablespoon'] = "a tablespoon is 3 teaspoons. You can most often use a normal spoon to replicate this measurement.";
+measurements['fluid ounce'] = "a fluid ounce is 2 tablespoons of liquid material";
+measurements['cup'] = "a cup is 8 fluid ounces";
+measurements['pint'] = "a pint is about 2 cups or 16 fluid ounces";
+measurements['quart'] = "a quart is 2 pints or 32 fluid ounces";
+measurements['gallon'] = "a gallon is 4 quarts, 128 ounces, or just under 16 cups";
+measurements['gram'] = "a gram is a standard unit of mass measurement in the international System of Units";
+measurements['kilogram'] = "a kilogram is 1000 grams";
+measurements['pound'] = "a pound is 16 ounces";
+measurements['inch'] = "an inch is 2.54 centimeters or roughly the distance from the top knuckle on your thumb to your thumb tip. ";
+measurements['cube'] = "a cube is a block of something with six sides. For example, a sugar cube is a cube of sugar.";
+measurements['to taste'] = "to taste is a method of adding flavor that requires you to frequently taste the result and adjust to your liking";
